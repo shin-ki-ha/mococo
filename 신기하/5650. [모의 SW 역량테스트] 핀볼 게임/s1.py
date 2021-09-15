e7b6ff017cc16d2c6c4f0dce95bcfee3750f0f2d
kiha <rlgktls@naver.com>
2021-09-15 17:50:59 +0900

    #5 핀볼 게임 풀이

--- 신기하/5650. [모의 SW 역량테스트] 핀볼 게임/s1.py
+++ 신기하/5650. [모의 SW 역량테스트] 핀볼 게임/s1.py
@@ -1,86 +1,135 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-def power_set(visit, n, k):
-    global min_time
-
-    if n == k:
-        dis_list = [0]
-        for i in range(1, k):
-            distance = abs(person[i][0] - step[visit[i]][0]) + abs(person[i][1] - step[visit[i]][1])
-            dis_list.append([distance, visit[i]])
-        i = 1
-        stack = [0, [], []]
-        check_person = [0] * (k - 1)
-
-        while 1:
-            if 0 not in check_person and len(stack[1]) == 0 and len(stack[2]) == 0:
-                break
-
-            for s in range(1, 3):
-                if len(stack[s]) == 1:
-                    stack[s][0][1] += 1
-                    if stack[s][0][1] == step[s][2]:
-                        stack[s].pop(0)
-
-                elif len(stack[s]) == 2:
-                    stack[s][0][1] += 1
-                    stack[s][1][1] += 1
-                    if stack[s][1][1] == step[s][2]:
-                        stack[s].pop(1)
-                    if stack[s][0][1] == step[s][2]:
-                        stack[s].pop(0)
-
-                elif len(stack[s]) >= 3:
-                    stack[s][0][1] += 1
-                    stack[s][1][1] += 1
-                    stack[s][2][1] += 1
-                    if stack[s][2][1] == step[s][2]:
-                        stack[s].pop(2)
-                    if stack[s][1][1] == step[s][2]:
-                        stack[s].pop(1)
-                    if stack[s][0][1] == step[s][2]:
-                        stack[s].pop(0)
-
-            for idx in range(1, k):
-                if len(dis_list[idx]) > 1:
-                    d, step_num = dis_list[idx]
-                    if d <= i and check_person[idx - 1] == 0:
-                        stack[step_num].append([idx, 0])
-                        check_person[idx - 1] = 1
-
-            i += 1
-        if i < min_time:
-            min_time = i
-            return
-        if i >= min_time:
-            return
-    visit[n] = 1
-    power_set(visit, n + 1, k)
-    visit[n] = 2
-    power_set(visit, n + 1, k)
-
 T = int(input())
 
 for tc in range(1, T + 1):
     N = int(input())
     matrix = [list(map(int, input().split())) for _ in range(N)]
-    person = [0]
-    step = [0]
-    min_time = 99999999999
+    max_score = 0
+
+    ny = [-1, 1, 0, 0]
+    nx = [0, 0, -1, 1]
 
     for c in range(N):
         for r in range(N):
-            if matrix[c][r] == 0:
-                continue
-            if matrix[c][r] == 1:
-                person.append([c, r])
-            else:
-                step.append([c, r, matrix[c][r]])
+            if matrix[c][r] == 0: # 구슬 놓을 위치 찾기
+                for m in range(4): # 상하좌우 방향 이동
+                    score = 0
+                    go_x, go_y = r, c
+                    mode = m # mode가 0 : 상, 1 : 하, 2 : 좌, 3 : 우
+                    while 1:
+                        go_y = go_y + ny[mode]
+                        go_x = go_x + nx[mode]
 
-    visit = [0 for _ in range(len(person))]
+                        # 시작 위치로 되돌아오면 끝
+                        if go_y == c and go_x == r:
+                            if score > max_score:
+                                max_score = score
+                            break
 
-    power_set(visit, 1, len(visit))
+                        if go_y == -1: # 위에 벽 부딪침
+                            mode = 1
+                            score += 1
+                            continue
 
-    print('#{} {}'.format(tc, min_time))
+                        elif go_y == N: # 밑에 벽 부딪침
+                            mode = 0
+                            score += 1
+                            continue
 
+                        elif go_x == -1: # 왼벽 부딪침
+                            mode = 3
+                            score += 1
+                            continue
+
+                        elif go_x == N: # 오른벽 부딪침
+                            mode = 2
+                            score += 1
+                            continue
+
+                        if matrix[go_y][go_x] == 0: # 통로인 경우 진행
+                            continue
+                        
+                        # 사물에 부딪치는 경우
+                        elif matrix[go_y][go_x] == 1:
+                            if mode == 0:
+                                mode = 1
+                            elif mode == 1:
+                                mode = 3
+                            elif mode == 2:
+                                mode = 0
+                            else:
+                                mode = 2
+                            score += 1
+                            continue
+
+                        elif matrix[go_y][go_x] == 2:
+                            if mode == 0:
+                                mode = 3
+                            elif mode == 1:
+                                mode = 0
+                            elif mode == 2:
+                                mode = 1
+                            else:
+                                mode = 2
+                            score += 1
+                            continue
+
+                        elif matrix[go_y][go_x] == 3:
+                            if mode == 0:
+                                mode = 2
+                            elif mode == 1:
+                                mode = 0
+                            elif mode == 2:
+                                mode = 3
+                            else:
+                                mode = 1
+                            score += 1
+                            continue
+
+                        elif matrix[go_y][go_x] == 4:
+                            if mode == 0:
+                                mode = 1
+                            elif mode == 1:
+                                mode = 2
+                            elif mode == 2:
+                                mode = 3
+                            else:
+                                mode = 0
+                            score += 1
+                            continue
+
+                        elif matrix[go_y][go_x] == 5:
+                            if mode == 0:
+                                mode = 1
+                            elif mode == 1:
+                                mode = 0
+                            elif mode == 2:
+                                mode = 3
+                            else:
+                                mode = 2
+                            score += 1
+                            continue
+                        
+                        # 블랙홀에 들어가는 경우
+                        elif matrix[go_y][go_x] == -1:
+                            if score > max_score:
+                                max_score = score
+                            break
+                        
+                        # 웜홀에 들어가는 경우 => 반대편 웜홀 찾아서 내보내기
+                        elif matrix[go_y][go_x] >= 6:
+                            hole = matrix[go_y][go_x]
+                            check_hole = False
+                            for i in range(N):
+                                for j in range(N):
+                                    if matrix[i][j] == hole:
+                                        if i != go_y or j != go_x:
+                                            go_y, go_x = i, j
+                                            check_hole = True
+                                            break
+                                if check_hole:
+                                    break
+
+    print('#{} {}'.format(tc, max_score))
