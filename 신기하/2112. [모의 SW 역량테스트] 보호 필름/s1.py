f1f273d1e3a4a4d876f22e298de528d674ce487f
kiha <rlgktls@naver.com>
2021-10-18 23:22:47 +0900

    #11 보호 필름 풀이

--- 신기하/2112. [모의 SW 역량테스트] 보호 필름/s1.py
+++ 신기하/2112. [모의 SW 역량테스트] 보호 필름/s1.py
@@ -1,57 +1,49 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-def cafe(mode, first, x, y, f_x, f_y, stack):
-    global total
-    # mode (우하), (좌하), (좌상), (우상)
-    if 0 <= x < N and 0 <= y < N:
-        if mode == 2:
-            if y <= f_y:
-                return
-
-        if mode == 3:
-            if x == f_x and y == f_y:
-                result = len(stack)
-                total = max(total, result)
-                return
-            elif x >= f_x or y <= f_y:
-                return
-
-        now = matrix[y][x]
-        if now in stack:
-            return
-        else:
-            stack.append(now)
-            if first:
-                cafe(0, False, x + 1, y + 1, f_x, f_y, stack)
+def checkFilm(film):
+    for x in range(W):
+        cnt = 0
+        now = film[0][x]
+        for y in range(1, D):
+            if film[y][x] == now:
+                cnt += 1
             else:
-                if mode == 0:
-                    cafe(0, first, x + 1, y + 1, f_x, f_y, stack)
-                    cafe(1, first, x - 1, y + 1, f_x, f_y, stack)
-                    stack.pop()
-                elif mode == 1:
-                    cafe(1, first, x - 1, y + 1, f_x, f_y, stack)
-                    cafe(2, first, x - 1, y - 1, f_x, f_y, stack)
-                    stack.pop()
-                elif mode == 2:
-                    cafe(2, first, x - 1, y - 1, f_x, f_y, stack)
-                    cafe(3, first, x + 1, y - 1, f_x, f_y, stack)
-                    stack.pop()
+                if cnt >= K - 1:
+                    continue
                 else:
-                    cafe(3, first, x + 1, y - 1, f_x, f_y, stack)
-                    stack.pop()
-    else:
+                    cnt = 0
+                    now = film[y][x]
+        if cnt < K - 1:
+            return False
+    return True
+
+def makeFilm(cnt, idx):
+    global result
+    if cnt >= result:
         return
 
-T = int(input())
+    if checkFilm(films):
+        result = min(result, cnt)
 
-for tc in range(1, T + 1):
-    N = int(input())
-    matrix = [list(map(int, input().split())) for _ in range(N)]
-    total = 0
-    for c in range(N - 1):
-        for r in range(1, N - 1):
-            cafe(0, True, r, c, r, c, [])
-    if total == 0:
-        total = -1
-    print('#{} {}'.format(tc, total))
+    for i in range(idx + 1, D):
+        layer = films[i][:]
+        for j in range(W):
+            films[i][j] = 0
+        makeFilm(cnt + 1, i)
+
+        for j in range(W):
+            films[i][j] = 1
+        makeFilm(cnt + 1, i)
+
+        for j in range(W):
+            films[i][j] = layer[j]
+
+
+
+for tc in range(1, int(input()) + 1):
+    D, W, K = map(int, input().split())
+    films = list(list(map(int, input().split())) for _ in range(D))
+    result = K
+    makeFilm(0, -1)
+    print('#{} {}'.format(tc, result))
